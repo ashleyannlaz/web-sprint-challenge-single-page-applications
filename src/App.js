e695ba08c79b41ea1f574461262fd71156843f3c
ashleyannlaz <paperpastels@gmail.com>
2021-07-23 10:27:10 -0400

    Created Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
-export default App;
+    
+    <div>
+      Hello World!
+    </div>
+  )
+}
+
+export default App
